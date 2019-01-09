7fb58a2cffb563a770e0d318a119d373f9394352
Diogo Gomes Viana <dgomesvi@everis.com>
2019-01-09 17:11:43 -0200

    Definição de login e senha para banco de dados e criação e configuração de ambiente de produção

--- Vagrantfile
+++ Vagrantfile
@@ -1,9 +1,22 @@
-
 Vagrant.configure("2") do |config|
- 
   config.vm.box = "hashicorp/precise32"
+
   config.vm.define :web do |web_config|
     web_config.vm.network "private_network", ip: "192.168.50.10"
     web_config.vm.provision "shell", inline: "sudo apt-get update && sudo apt-get install -y puppet"
   end
+
+#Configuração necessária para usar a interface gráfica!
+  config.vm.provider :virtualbox do |vb|
+   #vb.gui = true
+    vb.memory = 2024
+    vb.cpus =1
+  end
+
+  config.vm.define :prod do |prod_config|
+    prod_config.vm.box = "centos/7"
+    prod_config.vm.network "private_network", ip: "192.168.50.11"
+    prod_config.vm.provision "shell", inline: "sudo apt-get update && sudo apt-get install -y puppet"
+  end
 end
+
--- manifests/web.pp
+++ manifests/web.pp
@@ -1,14 +1,17 @@
 exec {"apt-update":
     command => "/usr/bin/apt-get update"
 }
+
 package {["openjdk-7-jre", "tomcat7", "mysql-server"]:
     ensure => installed,
     require => Exec["apt-update"]
 }
+
 package {'unzip':
     ensure => installed,
     require => Exec["apt-update"]
 }
+
 service {"tomcat7":
     ensure => running,
     enable => true,
@@ -16,13 +19,15 @@ service {"tomcat7":
     hasrestart => true,
     require => package["tomcat7"]
 }
+
 service {"mysql":
     ensure => running,
     enable => true,
     hasstatus => true,
-    hasrestart =>true,
+    hasrestart => true,
     require => package["mysql-server"]
 }
+
 file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
     source => "/vagrant/manifests/vraptor-musicjungle.war",
     owner => "tomcat7",
@@ -31,9 +36,29 @@ file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
     require => Package["tomcat7"],
     notify => Service["tomcat7"]
 }
+
 exec {"musicjungle":
     command =>"mysqladmin -uroot create musicjungle",
     unless =>"mysql -u root musicjungle",
     path =>"/usr/bin",
     require => Service["mysql"]    
 }
+
+exec{"mysql-password":
+    command =>"mysql -uroot -e\"GRANT ALL PRIVILEGES ON * TO 'musicjungle'@'%' IDENTIFIED BY 'minha-senha';" musicjungle 
+    unless =>"mysql -umusicjungle -pminha-senha musicjungle",
+    path =>"/urs/bin",
+    require => Exec["musicjungle"]
+}
+file_line {"production":
+    file => "/etc/default/tomcat7",
+    line => "JAVA_OPTS=\"\$JAVA_OPTS -Dbr.com.caelum.vraptor.environment=production\"",
+    require => Package["tomcat7"],
+    notify => Service["tomcat7"]
+}
+define file_line($file, $line) {
+    exec {"/bin/echo '$line' >> '$file'":
+        unless => "/bin/grep -qfx '$(line) '$(file)'"
+    }
+}
+
