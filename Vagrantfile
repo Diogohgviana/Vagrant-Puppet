d60a37c57c78e655a0404c3d770b87c26b1340d5
Diogo Gomes Viana <dgomesvi@everis.com>
2019-01-15 15:52:10 -0200

    Deploy na AWS EC2

--- Vagrantfile
+++ Vagrantfile
@@ -1,25 +1,52 @@
-Vagrant.configure("2") do |config|
-  config.vm.box = "hashicorp/precise32"
+  Vagrant.configure("2") do |config|
 
+    config.vm.provider "virtualbox"
+    config.vm.provider "aws"
+
+  #Dados do access key
+    config.vm.provider :aws do |aws, override|
+
+      aws.access_key_id = "AKIAI7S6IWO32IYDMSOA"
+      aws.secret_access_key = "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"
+
+     #Identificação da AMI
+      aws.ami = "ami-0f9cf087c1f27d9b1"
+      #Nome Security Group
+      aws.security_groups = ['devops-vagrant']
+      #Nome Arquivo.pem
+      aws.keypair_name = "devops-key"
+      #Nome do usuario (para S.O Ubuntu é ubuntu)
+      override.ssh.username = "ubuntu"
+      #Caminho e nome do arquivo pem
+      override.ssh.private_key_path = "devops-key.pem"
+    end
+
+  #Novo ambiente aws_web com o puppet
+  config.vm.define :aws_web do |aws_web_config|
+
+    aws_web_config.vm.box ="dummy"
+    aws_web_config.vm.synced_folder '.', '/vagrant', type: "rsync"
+
+    aws_web_config.vm.provider :aws do |aws|
+      aws.tags = {'Name' => 'MusicJungle (vagrant)'}
+    end
+
+    #Shell e Puppet provisioner
+    aws_web_config.vm.provision "shell", path: "manifests/bootstrap.sh"
+    aws_web_config.vm.provision "puppet" do |puppet|
+      puppet.manifest_file = "web.pp"
+      puppet.synced_folder_type = 'rsync'
+    end
+  end
+       
   config.vm.define :web do |web_config|
+    #usando ubuntu
+    web_config.vm.bom ="ubuntu/bionic64"
     web_config.vm.network "private_network", ip: "192.168.50.10"
-    web_config.vm.provision "shell", inline: "sudo apt-get update && sudo apt-get install -y puppet"
+    web_config.vm.provision "shell", path: "manifests/bootstrap.sh"
     web_config.vm.provision "puppet" do |puppet|
       puppet.manifest_file = "web.pp"
     end
   end
 
-#Configuração necessária para usar a interface gráfica!
-  config.vm.provider :virtualbox do |vb|
-   #vb.gui = true
-    vb.memory = 2024
-    vb.cpus =1
-  end
-
-  config.vm.define :prod do |prod_config|
-    prod_config.vm.box = "centos/7"
-    prod_config.vm.network "private_network", ip: "192.168.50.11"
-    #prod_config.vm.provision "shell", inline: "sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm  && sudo yum install -y puppet"
-  end
 end
-
--- manifests/web.pp
+++ manifests/web.pp
@@ -1,64 +1,52 @@
-exec {"apt-update":
-    command => "/usr/bin/apt-get update"
+exec { "apt-update":
+	command => "/usr/bin/apt-get update"
 }
 
-package {["openjdk-7-jre", "tomcat7", "mysql-server"]:
-    ensure => installed,
-    require => Exec["apt-update"]
+package { ["vim", "openjdk-8-jre", "tomcat8", "mariadb-server"]:
+	ensure => installed,
+	require => Exec["apt-update"]
 }
 
-package {'unzip':
-    ensure => installed,
-    require => Exec["apt-update"]
-}
-
-service {"tomcat7":
+service { "mariadb":
     ensure => running,
     enable => true,
     hasstatus => true,
     hasrestart => true,
-    require => package["tomcat7"]
+    require => Package["mariadb-server"]
 }
 
-service {"mysql":
-    ensure => running,
-    enable => true,
-    hasstatus => true,
-    hasrestart => true,
-    require => package["mysql-server"]
+exec { "musicjungle":
+    command => "mysqladmin -uroot create musicjungle",
+    unless => "mysql -u root musicjungle",
+    path => "/usr/bin",
+    require => Service["mariadb"]
 }
 
-file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
-    source => "/vagrant/manifests/vraptor-musicjungle.war",
-    owner => "tomcat7",
-    group => "tomcat7",
-    mode => 0644,
-    require => Package["tomcat7"],
-    notify => Service["tomcat7"]
-}
-
-exec {"musicjungle":
-    command =>"mysqladmin -uroot create musicjungle",
-    unless =>"mysql -u root musicjungle",
-    path =>"/usr/bin",
-    require => Service["mysql"]    
-}
-
-exec{"mysql-password":
-    command =>"mysql -uroot -e\"GRANT ALL PRIVILEGES ON * TO 'musicjungle'@'%' IDENTIFIED BY 'minha-senha';" musicjungle 
-    unless =>"mysql -umusicjungle -pminha-senha musicjungle",
-    path =>"/urs/bin",
+exec { "user_mariadb":
+    command => "/bin/echo \"CREATE USER 'admin'@'localhost';GRANT USAGE on *.* TO 'admin'@'localhost';GRANT ALL privileges ON musicjungle.* TO  'admin'@'localhost';FLUSH PRIVILEGES;\" |  /usr/bin/mysql -u root",
     require => Exec["musicjungle"]
 }
-file_line {"production":
-    file => "/etc/default/tomcat7",
-    line => "JAVA_OPTS=\"\$JAVA_OPTS -Dbr.com.caelum.vraptor.environment=production\"",
-    require => Package["tomcat7"],
-    notify => Service["tomcat7"]
-}
-define file_line($file, $line) {
-    exec {"/bin/echo '$line' >> '$file'":
-        unless => "/bin/grep -qfx '$(line) '$(file)'"
-    }
+
+service { "tomcat8":
+	ensure => running,
+	enable => true,
+	hasstatus => true,
+	hasrestart => true,
+	require => Package["tomcat8"]
 }
 
+file { "/var/lib/tomcat8/webapps/vraptor-musicjungle.war":
+	source => "/vagrant/manifests/vraptor-musicjungle.war",
+	owner => "tomcat8",
+	group => "tomcat8",
+	mode => "0644",
+	require => Package["tomcat8"],
+	notify => Service["tomcat8"]
+}
+
+file_line { "production":
+    path => "/etc/default/tomcat8",
+    line => "JAVA_OPTS=\"\$JAVA_OPTS -Dbr.com.caelum.vraptor.environment=production\"",
+    require => Package["tomcat8"],
+    notify => Service["tomcat8"]
+}
